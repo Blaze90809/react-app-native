b07f90f7b203ba89192c549b1eb3b694cedf0e50
Blaze Fugina <blaze.fugina@gmail.com>
2018-08-08 22:28:36 -0500

    Working to implement react native.

--- app/src/Home.js
+++ app/src/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import {View, Text, Button } from 'react-native'
+import {View, Text, Button, StyleSheet } from 'react-native';
+import { Toolbar } from 'react-native-material-ui';
+
 
 
 export default ({ history }) => (
-            <View>
-                <Text>This is the home page.</Text>
-                <Button title="change page" onPress={() => history.push("/table")} /> 
-            </View>
-        );
+        // <View style={{ flex: 1}}>
+            
+        // </View>
+        <View style={ styles.container }>
+        <Toolbar leftElement="menu" />
+            <Text>Hello</Text>
+            <Text>This is the home page.</Text>
+            <Button title="change page" onPress={() => history.push("/table")} />
+        </View>
+);
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+    },
+});
